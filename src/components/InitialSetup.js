6c537f4175e01b8a0e798e9181f3d6019dd327d9
Alexandra Neagu <alexandra.neagu@ASCENDRO.LOCAL>
2023-06-21 13:51:29 +0300

    fixed the start of the game- in initial setup

--- src/components/InitialSetup.js
+++ src/components/InitialSetup.js
@@ -1,55 +1,66 @@
-import React, {useState} from 'react';
-import {TextField,Button} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { TextField, Button } from '@mui/material';
 import './initialSetup.css';
 
-function InitialSetup({handleStartGame}){
+function InitialSetup({ handleStartGame }) {
+  const [playerName, setPlayerName] = useState('');
+  const [playerName2, setPlayerName2] = useState('');
+  const [finalValue, setFinalValue] = useState('');
+  const [finalValue2, setFinalValue2] = useState('');
 
-    
-    const[playerName, setPlayerName]=useState('');
-    const[playerName2, setPlayerName2]=useState('');
-    const[finalValue, setFinalValue]= useState('');
-    const[finalValue2, setFinalValue2]= useState('');
+  const handleInputChange = (event) => {
+    setPlayerName(event.target.value);
+  };
 
-    
-    const handleInputChange = (event) => {
-        setPlayerName(event.target.value);
-      };
+  const handleInputChange2 = (event) => {
+    setPlayerName2(event.target.value);
+  };
 
-      const handleInputChange2 = (event) => {
-        setPlayerName2(event.target.value);
-      };
+  const handleSave = () => {
+    setFinalValue(playerName);
+  };
 
-      const handleSave = () => {
-        setFinalValue(playerName)
-        // console.log('Player 1: ', playerName);
-      };  
+  const handleSave2 = () => {
+    setFinalValue2(playerName2);
+  };
 
-      const handleSave2 = () => {
-        setFinalValue2(playerName2)
-        // console.log('Player 2: ', playerName2);
-      };
+  useEffect(() => {
+    if (finalValue && finalValue2) {
+      handleStartGame();
+    }
+  }, [finalValue, finalValue2, handleStartGame]);
 
-      const handleSubmit = () => {
-        handleStartGame();
-      }
-
-    return(
-        <span >Hanging Man
-                <div classsname="input">
-                    <TextField id="outlined-basic" label="Name" variant="outlined" value={playerName} onChange={handleInputChange} />
-                    <Button variant="outlined" onClick={handleSave}>Submit</Button>
-                    <TextField id="outlined-basic" label="Name" variant="outlined" value={playerName2} onChange={handleInputChange2} />
-                    <Button variant="outlined" onClick={handleSave2}>Submit</Button>
-                    <Button onClick={handleSubmit}>Start Game</Button>
-                </div>
-                <div classsname="input">
-                <p>Player 1:{finalValue}</p>
-                <p>Player 2:{finalValue2}</p>
-                </div>
-        </span>
-    )
+  return (
+    <span>
+      Hanging Man
+      <div className="input">
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          value={playerName}
+          onChange={handleInputChange}
+        />
+        <Button variant="outlined" onClick={handleSave}>
+          Submit
+        </Button>
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          value={playerName2}
+          onChange={handleInputChange2}
+        />
+        <Button variant="outlined" onClick={handleSave2}>
+          Submit
+        </Button>
+      </div>
+      <div className="input">
+        <p>Player 1: {finalValue}</p>
+        <p>Player 2: {finalValue2}</p>
+      </div>
+    </span>
+  );
 }
-
-
 
 export default InitialSetup;
